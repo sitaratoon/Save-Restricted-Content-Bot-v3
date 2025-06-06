cad618c71fe2da6c50ecd06a585e8edc742df37a
sitaratoon <ajay393798@gmail.com>
2025-06-06 11:03:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,6 +32,6 @@ YT_COOKIES = os.getenv("YT_COOKIES", YTUB_COOKIES)
 INSTA_COOKIES = os.getenv("INSTA_COOKIES", INST_COOKIES)
 FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "500"))
 PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", "500"))
-JOIN_LINK = os.getenv("JOIN_LINK", "https://t.me/team_spy_pro") # this link for start command message
-ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/username_of_admin")
+JOIN_LINK = os.getenv("JOIN_LINK", "https://t.me/ST_Rename_Update") # this link for start command message
+ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/sitaratoons_support")
 
